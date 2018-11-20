fcddb751d353bf6b30051a5cd267e43eea100ec9
Mateus Vieira Machado <mateusvmachado@outlook.com>
2018-11-20 14:16:38 -0200

    Adicionado Corte X activity

--- app/src/main/java/com/r2h2solutions/corner/ativities/CorteXActivity.java
+++ app/src/main/java/com/r2h2solutions/corner/ativities/CorteXActivity.java
@@ -2,31 +2,25 @@ package com.r2h2solutions.corner.ativities;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
-import android.view.View;
 
 import com.r2h2solutions.corner.R;
 
 /**
- * Classe para definições da Activity de corte Degrade.
+ * Classe para definições da Activity dentro de um corte.
  *
  * @author Mateus Vieira Machado
  * @version 1.0.0
  * @since 1.0.0
  */
-public class DegradeActivity extends BottomNavigationActivity {
+public class CorteXActivity extends BottomNavigationActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_degrade);
+        setContentView(R.layout.activity_cortex);
 
         BottomNavigationView navigation = findViewById(R.id.navigation_bottom);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
-    public void abreCorteX(View view) {
-
-        openIntent(CorteXActivity.class);
     }
 }
