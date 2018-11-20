249fef6dec27cf1dbb5f0ab2b532f411eaa5f959
Mateus Vieira Machado <mateusvmachado@outlook.com>
2018-11-19 23:26:26 -0200

    Initial Commit

---
+++ app/src/main/java/com/r2h2solutions/corner/ativities/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.r2h2solutions.corner.ativities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.r2h2solutions.corner.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
