b1126c366ca7d42026821249d0a97a1c6990f299
Mateus Vieira Machado <mateusvmachado@outlook.com>
2018-11-20 13:38:25 -0200

    Adicionado Degrade activity

--- app/src/main/java/com/r2h2solutions/corner/ativities/DegradeActivity.java
+++ app/src/main/java/com/r2h2solutions/corner/ativities/DegradeActivity.java
@@ -2,38 +2,25 @@ package com.r2h2solutions.corner.ativities;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
-import android.view.View;
 
 import com.r2h2solutions.corner.R;
 
 /**
- * Classe para definições da Activity de Portfólio.
+ * Classe para definições da Activity de corte Degrade.
  *
  * @author Mateus Vieira Machado
  * @version 1.0.0
  * @since 1.0.0
  */
-public class PortfolioActivity extends BottomNavigationActivity {
+public class DegradeActivity extends BottomNavigationActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_portfolio);
+        setContentView(R.layout.activity_degrade);
 
         BottomNavigationView navigation = findViewById(R.id.navigation_bottom);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
-
-    public void abrirDegrade(View view) {
-
-        openIntent(DegradeActivity.class);
-    }
-
-    @Override
-    protected void onPause() {
-
-        super.onPause();
-        finish();
     }
 }
